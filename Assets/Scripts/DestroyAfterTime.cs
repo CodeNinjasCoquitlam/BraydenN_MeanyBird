e8467060c5fd2a831fef4b5539845005602bdd44
CodeNinjasCoquitlam <coquitlamninja@gmail.com>
2025-04-09 17:18:56 -0700

    April9

--- Assets/Scripts/DestroyAfterTime.cs
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class DestroyAfterTime : MonoBehaviour
 {
+    [Header("Default Destruction Time")]
+    public float timeToDestruction;
     // Start is called before the first frame update
     void Start()
     {
-
+        Invoke("DestroyObject", timeToDestruction);
     }
 
     // Update is called once per frame
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
     void Update()
     {
 
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+
     [Header("Game Controller Object for controlling the game")]
     public GameController gameController;
-    [Header("Velocity")]
+    [Header("Default Score")]
     public float velocity = 5;
     private Rigidbody2D rb;
     private float objectHeight;
+
+    public GameController GameController { get => gameController; set => gameController = value; }
+
     // Start is called before the first frame update
     void Start()
     {
-        gameController = GetComponent<GameController>();
+        GameController = GetComponent<GameController>();
         Time.timeScale = 1;
         rb = GetComponent<Rigidbody2D>();
-        objectHeight = transform.GetComponent<SpriteRenderer>();.bounds.size.y /2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * velocity;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D Collision)
+    {
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+        if(Collision.gameObject.tag == "HighSpike" || Collision.gameObject.tag == "lowSpike" || Collision.gameObject.tag == "Ground")
+        {
+            Time.timeScale = 0;
         }
     }
 }
